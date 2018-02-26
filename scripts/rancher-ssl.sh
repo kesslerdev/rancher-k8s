50c49ed8895c6e350f2f43501d9331c3f43ca771
Kessler Dev <kessler.dev@gmail.com>
2018-02-26 19:33:05 +0100

    use relative directory for rancher ssl script add also non interactive mode for certbot

--- scripts/rancher-ssl.sh
+++ scripts/rancher-ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Indique au système que l'argument qui suit est le programme utilisé pour exécuter ce fichier
-# En règle générale, les "#" servent à mettre en commentaire le texte qui suit comme ici
-echo Install ssl
+
+curdir="${0%/*}"
+echo Install rancher with ssl
 
 echo "Type your email (for let's encrypt), followed by [ENTER]:"
 read email
@@ -25,12 +25,12 @@ echo setup letsencrypt
 
 wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-./certbot-auto
+./certbot-auto -n
 
-./certbot-auto certonly --standalone --agree-tos -m $email -d $domain
+./certbot-auto certonly -n --standalone --agree-tos -m $email -d $domain
 
 echo Write nginx conf
-sed -e "s/<SERVER_NAME>/$domain/g" ../templates/nginx.conf | sed "s/<DOMAIN>/$domain/g" > /etc/nginx.conf
+sed -e "s/<SERVER_NAME>/$domain/g" $curdir/../templates/nginx.conf | sed "s/<DOMAIN>/$domain/g" > /etc/nginx.conf
 
 echo Starting rancher
 docker run -d --name=rancher-server --restart=unless-stopped rancher/server:stable
